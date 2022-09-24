9dd32cf6b9996f59f575fc59ae5ccd7fc14d717c
amarn <user.name>
2022-09-24 21:19:41 +0530

    Microservice Initial Commit

---
+++ product-service-catalog/src/main/java/com/productservicecatalog/productservicecatalog/model/Product.java
@@ -0,0 +1,71 @@
+package com.productservicecatalog.productservicecatalog.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+
+	private String productName;
+	private BigDecimal price;
+	private String description;
+	private String category;
+	private int availability;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public int getAvailability() {
+		return availability;
+	}
+
+	public void setAvailability(int availability) {
+		this.availability = availability;
+	}
+
+}
